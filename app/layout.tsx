982fa66c83cd4242d1256d80d8998aacb2b90cdc
Mohamed Gad <gad254530@gmail.com>
2024-09-27 16:45:38 +0300

    redux configurations

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,25 @@
-import { Inter } from 'next/font/google'
-import { ThemeProvider } from "@/components/base/ThemeProvider"
-import { MainNav } from "@/components/shared/Header/MainNav"
-import "@/app/globals.css"
+import { Inter } from "next/font/google";
+import { MainNav } from "@/components/shared/Header/MainNav";
+import "@/app/globals.css";
+import MainProvider from "@/components/providers";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem={false}
-          disableTransitionOnChange
-        >
+        <MainProvider>
           <div className="flex flex-col min-h-screen">
             <MainNav />
-            <main className="flex-1">
-              {children}
-            </main>
+            <main className="flex-1">{children}</main>
           </div>
-        </ThemeProvider>
+        </MainProvider>
       </body>
     </html>
-  )
+  );
 }
